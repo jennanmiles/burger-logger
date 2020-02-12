5b48acfdd12cfe0cc91c76cecff247b83d1b8061
jennanmiles <jennanmiles@gmail.com>
2020-02-11 22:28:06 -0700

    adding routes

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,32 @@
 let connection = require('../config/connection.js');
 
 let orm = {
+    selectAll: function(burgerName, cb) {
+        let queryStr = 'SELECT * FROM burgers_db';
+        connection.query(queryStr, [burgerName, cb], function(err,result) {
+            if (err) throw err; 
+            console.log(results);
+        });
+    },
+    insertOne: function (burgerName, cb) {
+        let queryStr = 'INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)';
+        console.log(queryStr);
 
+        connection.query(queryStr, [burgerName, cb], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(burgerName,update) {
+        let queryStr = 'SELECT ? FROM burgers_db WHERE ?';
+
+        console.log(queryStr);
+
+        connection.query(queryStr, [burgerName, update], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
 }; 
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,26 @@
 let orm = require('../config/orm.js');
 
 let burger = {
-
+    all: function(cb) {
+        orm.all('burgers', function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create('burgers', cols, cals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete('burgers', condition, function(res) {
+            cb(res);
+        });
+    }
 };
 
 // export database functions for controller
